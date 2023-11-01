073b6d2f0ae6978702e5a67701df8713c7608589
Jason <jason.chrispen@gmail.com>
2023-10-31 23:28:53 -0400

    Added giant food support.

--- src/js/sites/giantfood.js
+++ src/js/sites/giantfood.js
@@ -7,119 +7,127 @@
 /**
  * static constant values for all scripts
  */
-const GIANT_URL = "https://www.giantfoods.com";
+const GIANT_URL = "https://giantfood.com";
 
 /** ---------------------------------------------------------------------------
  * GiantFood.com background functions
  **/
 async function giant_background(tab) {
+    let userUuid;
+    let userId;
+    let locationId;
     try {
-        let membershipNumber;
-        try {
-            membershipNumber = await giant_getMembershipNumber(tab)
-                .then(member_number => {
-                    return member_number;
-                });
-        } catch (error) {
-            onError(error);
-            return rejectWith("Need to login");
-        }
-        const clubDetails = await giant_getClubDetails(tab);
-        const zipcode = await giant_parseZipcode(clubDetails);
-        return await giant_clipCoupons(tab, membershipNumber, zipcode);
+        userUuid = await giant_getUserUuid(tab)
+            .then(item => {
+                return item;
+            });
+        userId = await giant_getUserId(tab)
+            .then(item => {
+                return item;
+            });
+        locationId = await giant_getLocationId(tab)
+            .then(item => {
+                return item;
+            });
+
+        return await giant_clipCoupons(tab, userId, locationId);
     } catch (error) {
         return rejectWith(error.message);
     }
 }
 
-async function giant_getMembershipNumber(tab) {
-    console.log("Getting membership number");
-    const membershipNumber = await grabItem(tab, "x_MembershipNumber");
-    // if membership is null or undefined exit.
-    if (isInvalid(membershipNumber)) {
+async function giant_getUserUuid(tab) {
+    console.log("Getting user-uuid");
+    const uuid = await grabItem(tab, "user-uuid");
+    // if item is null or undefined throw down.
+    if (isInvalid(uuid)) {
         throw new Error("Need to login");
     }
 
-    console.log("MembershipNumber: " + membershipNumber);
-    return resolveWith(membershipNumber);
+    console.log("User-Uuid: " + uuid);
+    return resolveWith(uuid);
 }
 
-async function giant_getClubDetails(tab) {
-    console.log("Getting clubDetails");
-    const clubDetails = await grabItem(tab, "clubDetailsForClubId");
-
-    // if clubDetails is null or undefined exit.
-    if (isInvalid(clubDetails)) {
-        throw new Error("Need location selected to be able to pull offers");
+async function giant_getUserId(tab) {
+    console.log("Getting User ID");
+    const userId = await grabAppConfig(tab, "userId");
+    // if item is null or undefined throw down.
+    if (isInvalid(userId)) {
+        throw new Error("Need to login");
     }
 
-    return resolveWith(clubDetails);
+    console.log("User ID: " + userId);
+    return resolveWith(userId);
 }
 
-async function giant_parseZipcode(jsonString) {
-    console.log("Getting zipcode from json");
-    let postalCode = JSON.parse(jsonString)["postalCode"];
-    console.log("Zipcode: " + postalCode);
-    return resolveWith(postalCode);
+async function giant_getLocationId(tab) {
+    console.log("Getting locationId");
+    const locationId = await grabAppConfig(tab, "currentServiceLocationId");
+    // if item is null or undefined throw down.
+    if (isInvalid(locationId)) {
+        throw new Error("Need to login");
+    }
+
+    console.log("locationIdd: " + locationId);
+    return resolveWith(locationId);
 }
 
-async function giant_clipCoupons(tab, membershipNumber, zipcode) {
+async function giant_clipCoupons(tab, userId, locationId) {
     console.log("Send clip request");
     return browser.tabs.sendMessage(tab.id,
-        { type: Type.giant_clipOffers, membershipNumber: membershipNumber, zipcode: zipcode });
+        { type: Type.giant_clipOffers, userId: userId, locationId: locationId });
 }
 
 /** ---------------------------------------------------------------------------
  * BJs.com content functions
  **/
-
-async function giant_getItem(itemKey) {
-    const itemValue = getItem(itemKey);
-    return resolveWith({ item: itemValue });
-}
-
-async function giant_content(membershipNumber, zipcode) {
+async function giant_content(userId, locationId) {
     console.log("Preparing to clip offers");
-    return giant_fetch(membershipNumber, zipcode)
-        .then(response => {
-            return giant_processFetch(response);
-        })
+    return giant_fetch(userId, locationId)
         .then((availableOffers) => {
-            return giant_processOffers(availableOffers, zipcode);
+            return giant_processOffers(availableOffers, userId);
         });
 }
 
-async function giant_fetch(membershipNumber, zipcode) {
+async function giant_fetch(userId, locationId) {
     console.log("Fetching available offers from API");
-    return await fetch('https://api.bjs.com/digital/live/api/v1.0/member/available/offers', {
-        method: 'post',
-        credentials: 'include',
-        body: JSON.stringify({
-            membershipNumber,
-            zipcode,
-            'category': '',
-            'isPrev': false,
-            'isNext': true,
-            'pagesize': MAX_COUPON_REQUEST,
-            'searchString': '',
-            'indexForPagination': 0,
-            'brand': ''
-        })
-    })
-}
+    const fetch_url = `${GIANT_URL}/api/v6.0/coupons/users/${userId}/prism/service-locations/${locationId}/coupons/search?fullDocument=true&unwrap=true`;
 
-async function giant_processFetch(response) {
-    // Check for a failed response
-    if (response.status < 200 || response.status >= 300) {
-        throw new Error("Offers fetch unsuccessful [" + response.status + "] " + response.statusText);
+    try {
+        const response = await fetch(fetch_url, {
+            method: "post",
+            credentials: 'include',
+            mode: "cors",
+            body: JSON.stringify({
+                query: { start: 0, size: MAX_COUPON_REQUEST },
+                filter: { loadable: true, loaded: false, sourceSystems: ["QUO", "COP"] },
+                copientQuotientTargetingEnabled: true,
+                sorts: [{ targeted: "desc" }]
+            }),
+            headers: {
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/118.0",
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Language": "en-US,en;q=0.5",
+                "Content-Type": "application/json;charset=utf-8",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin"
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data.coupons;
+    } catch (error) {
+        onError(error);
+        throw error;
     }
-    console.log("Offers fetch status [" + response.status + "]");
-
-    const data = await response.json();
-    return data[0].availableOffers; // Return an object with the parsed JSON data
 }
 
-async function giant_processOffers(availableOffers, zipcode) {
+async function giant_processOffers(availableOffers, userId) {
     if (availableOffers.length === 0) {
         return resolveWith("No offers available");
     }
@@ -127,9 +135,8 @@ async function giant_processOffers(availableOffers, zipcode) {
     let count = 0;
     if (availableOffers && Array.isArray(availableOffers)) {
         for (const offer of availableOffers) {
-            const offerId = offer.offerId;
-            const storeId = offer.storeId;
-            if (await giant_fetchCoupons(offerId, storeId, zipcode)) {
+            const offerId = offer.id;
+            if (await giant_fetchCoupons(offerId, userId)) {
                 count++
             }
         }
@@ -139,14 +146,28 @@ async function giant_processOffers(availableOffers, zipcode) {
     return resolveWith(count + " coupons clipped");
 }
 
-async function giant_fetchCoupons(offerId, storeId, zipcode) {
+async function giant_fetchCoupons(offerId, userId) {
     if (isInvalid(offerId)) {
         onError("OfferId is not valid");
         return false;
     }
 
-    const url = `https://api.bjs.com/digital/live/api/v1.0/store/${storeId}/coupons/activate?zip=${zipcode}&offerId=${offerId}`;
-    console.log("Url: " + url);
-    const response = await fetch(url, {credentials: 'include'});
+    const response = await fetch(`${GIANT_URL}/api/v6.0/users/${userId}/coupons/clipped`, {
+        credentials: "include",
+        headers: {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/119.0",
+            "Accept": "application/json, text/plain, */*",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Content-Type": "application/json;charset=utf-8",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin"
+        },
+        referrer: `${GIANT_URL}/savings/coupons/browse`,
+        body: JSON.stringify({ couponId: offerId }),
+        method: "POST",
+        mode: "cors"
+    });
     return (response.status >= 200 && response.status <= 299);
 }
+
